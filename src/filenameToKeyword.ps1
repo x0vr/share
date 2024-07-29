422dfea0a066644fbf3942a0187c573b255ce6a7
x0v3rR <x0v3rr@gmail.com>
2024-07-29 21:26:05 +0200

    add description

--- src/filenameToKeyword.ps1
+++ src/filenameToKeyword.ps1
@@ -1,9 +1,6 @@
 # ---------------------------------------------------------------------------- #
 #                                  DESCRIPTION                                 #
-# This script is about adding the filename of a JPEG as metadata tag (IPTC)    #
-# ---------------------------------------------------------------------------- #
-
-
+# This script is about adding the filename of a JPEG as metadata tag (IPTC).   #
 # ---------------------------------------------------------------------------- #
 #                                  CONFIG HERE                                 #
 # ---------------------------------------------------------------------------- #
@@ -14,9 +11,28 @@ $folderPath = "E:\Bilder\tmp"
 $exifToolPath = "C:\Exiftool\exiftool.exe"
 
 # ---------------------------------------------------------------------------- #
-#                           INTERNAL - do not touch!                           #
+#                                 INSTRUCTIONS                                 #
+#                                                                              #
+# 1. Install ExifTool:                                                         #
+#   - Download ExifTool from the ExifTool website (https://exiftool.org/)      #
+#   - Extract or install it to a directory, e.g. C:\ExifTool\                  #
+#                                                                              #
+# 2. Save the Script:                                                          #
+#   - Copy this PowerShell script to your local harddisk                       #
+#                                                                              #
+# 3. Set up Paths                                                              #
+#   - Set variable $folderPath to the path of your JPG files                   #
+#   - Ensure that variable $exifToolPath is set to path of Step 1.             #
+#                                                                              #
+# 3. Run the Script:                                                           #
+#   - Open PowerShell ( you can use Win + X )                                  #
+#   - Navigate to the directory where you saved the script                     #
+#   - Execute the script by typing: .\filenameToKeyword.ps1                    #
 # ---------------------------------------------------------------------------- #
 
+# ---------------------------------------------------------------------------- #
+#                     INTERNAL - do not touch lines below!                     #
+# ---------------------------------------------------------------------------- #
 # Get all JPG files in the folder
 $jpgFiles = Get-ChildItem -Path $folderPath -Include *.jpg,*.jpeg,*.JPG,*.JPEG -Recurse -File
 
